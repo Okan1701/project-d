28898a048a37ae8a2e60030ec750925710eac9bc
Okan Emeni <okanemeni@outlook.com>
2019-05-10 15:31:35 +0200

    Updated django install scripts

--- backend/install.bat
+++ backend/install.bat
@@ -2,6 +2,9 @@
 pip3 install virtualenv
 virtualenv venv
 venv\Scripts\pip install -r requirements.txt
+venv\Scripts\python manage.py makemigrations api
 venv\Scripts\python manage.py migrate
+echo Please enter credentials of new admin account
+venv\Scripts\python manage.py createsuperuser
 echo Installatie klaar!
 pause
